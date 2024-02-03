e869a4e5ea9c3049cc2af0c2cd1ac0d7f717466a
hbs <hariprasad.b-s@dxc.com>
2024-02-03 20:17:58 +0530

    Scala Program on Branch

--- src/main/scala/Hariprasad.scala
+++ src/main/scala/Hariprasad.scala
@@ -2,5 +2,6 @@ object Hariprasad {
   def main(args: Array[String]): Unit = {
     println("Sri Haraye Namaha")
     println("Sri Lakshmi Narasimhaya Namaha")
+    println("Sri Keshavaya Namaha")
   }
 }
